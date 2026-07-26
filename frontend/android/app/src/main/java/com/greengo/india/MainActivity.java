d8b3e79a9df5979a03f257cc35944a6ca7884ba1
Prince Raj <princerajmne@gmail.com>
2026-07-26 16:18:10 +0530

    feat(android): add native crash dialog and bump version to 1.0.57 (code 58) to inspect notification screen crash

--- frontend/android/app/src/main/java/com/greengo/india/MainActivity.java
+++ frontend/android/app/src/main/java/com/greengo/india/MainActivity.java
@@ -12,6 +12,47 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final Thread.UncaughtExceptionHandler defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread thread, final Throwable throwable) {
+                try {
+                    java.io.StringWriter sw = new java.io.StringWriter();
+                    java.io.PrintWriter pw = new java.io.PrintWriter(sw);
+                    throwable.printStackTrace(pw);
+                    final String stackTrace = sw.toString();
+
+                    android.util.Log.e("GreenGoCrash", "FATAL EXCEPTION: " + stackTrace);
+
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            android.os.Looper.prepare();
+                            new android.app.AlertDialog.Builder(MainActivity.this)
+                                .setTitle("🚨 Native App Crash Detected")
+                                .setMessage(stackTrace)
+                                .setCancelable(false)
+                                .setPositiveButton("Close App", new android.content.DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(android.content.DialogInterface dialog, int which) {
+                                        System.exit(1);
+                                    }
+                                })
+                                .create()
+                                .show();
+                            android.os.Looper.loop();
+                        }
+                    }.start();
+
+                    Thread.sleep(60000);
+                } catch (Exception e) {
+                    if (defaultHandler != null) {
+                        defaultHandler.uncaughtException(thread, throwable);
+                    }
+                }
+            }
+        });
     }
 
     @Override
